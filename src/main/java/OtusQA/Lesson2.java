4a19dd20bebd67a642d2b2fd3f63e8394d6e972b
lubnin <lubnin@users.noreply.github.com>
2019-06-13 00:58:25 +0300

    all works but logging are not

--- src/main/java/OtusQA/Lesson2.java
+++ src/main/java/OtusQA/Lesson2.java
@@ -1,13 +1,26 @@
 package OtusQA;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Lesson2 {
+public class Lesson2
+{
+    static final Logger logger = LogManager.getLogger(Lesson2.class);
 
-public static void main (String[] args){
-    System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-    ChromeDriver driver = new ChromeDriver();
-    driver.get("http://www.otus.ru/");
-}
+    public void pageOpening()
+    {
+        logger.debug("Назначаем директорию с драйвером");
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+        logger.debug("Инициализируем драйвер");
+        ChromeDriver driver = new ChromeDriver();
+        logger.info("Развернём окно");
+        driver.manage().window().maximize();
+        logger.info("Переходим на страницу");
+        driver.get("http://www.otus.ru/");
+        driver.quit();
+        logger.warn("Страница закрылась слишком быстро( ");
+    }
 }
 
---
+++ src/test/java/OtusQA/LoggerTest.java
@@ -0,0 +1,11 @@
+package OtusQA;
+
+import org.junit.Test;
+
+public class LoggerTest {
+    @Test
+    public void testOtusPage(){
+        Lesson2 test = new Lesson2();
+        test.pageOpening();
+    }
+}
