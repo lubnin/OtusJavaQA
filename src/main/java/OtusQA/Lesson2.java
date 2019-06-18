c88b33009e2cd7aee718dccd39b4754fdce15b75
lubnin <lubnin@users.noreply.github.com>
2019-06-18 10:44:59 +0300

    Lesson 2 added Assertion and fixed driver  initialization

--- src/main/java/OtusQA/Lesson2.java
+++ src/main/java/OtusQA/Lesson2.java
@@ -1,9 +1,12 @@
 package OtusQA;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class Lesson2
 {
@@ -11,14 +14,16 @@ public class Lesson2
 
     public void pageOpening()
     {
-        logger.debug("Назначаем директорию с драйвером");
-        System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+        //logger.debug("Назначаем директорию с драйвером");
+        //System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
         logger.debug("Инициализируем драйвер");
+        WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         logger.info("Развернём окно");
         driver.manage().window().maximize();
         logger.info("Переходим на страницу");
         driver.get("http://www.otus.ru/");
+        Assert.assertEquals("OTUS - Онлайн-образование", driver.getTitle());
         driver.quit();
         logger.warn("Страница закрылась слишком быстро( ");
     }
